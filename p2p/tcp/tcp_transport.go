0546449938dffa95ca191a44f94c80345b92c6f4
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-06-30 14:42:10 +0800

    feat: add tcp accept & handle simple logic with Unit test

--- p2p/tcp/tcp_transport.go
+++ p2p/tcp/tcp_transport.go
@@ -1,6 +1,7 @@
 package tcp
 
 import (
+	"fmt"
 	"github.com/roylic/go-distributed-file-storage/p2p"
 	"net"
 	"sync"
@@ -17,4 +18,34 @@ func NewTCPTransport(listenAdd string) *TCPTransport {
 	return &TCPTransport{
 		listenAddress: listenAdd,
 	}
+}
+
+// ListenAndAccept 进行TCP连接的Accept启动
+func (t *TCPTransport) ListenAndAccept() error {
+	// 绑定port到listener
+	var err error
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+	// 另启线程, 开始循环accept
+	go t.startAcceptLoop()
+	return nil
+}
+
+// startAcceptLoop 循环监听Accept请求
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		// accept请求
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept errors:%s\n", err)
+		}
+		// 另起线程, 处理conn
+		go t.handleConn(conn)
+	}
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	fmt.Printf("new incoming connection %+v\n", conn)
 }
--- p2p/tcp/tcp_transport_test.go
+++ p2p/tcp/tcp_transport_test.go
@@ -6,8 +6,12 @@ import (
 )
 
 func Test_TCPTransport(t *testing.T) {
+
+	// check address
 	listenAddr := ":4000"
 	tr := NewTCPTransport(listenAddr)
-
 	assert.Equal(t, tr.listenAddress, listenAddr)
+
+	// check accept return no error
+	assert.Nil(t, tr.ListenAndAccept())
 }
